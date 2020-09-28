ef7dd4c1aac24e2ae9e80f78dadfc5787daf7778
dalsomin <dalsomin425@gmail.com>
2020-09-28 15:58:45 +0900

    second setting"

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {BrowerRouter, Route} from "react-router-dom";
+import "semantic-ui-css/semantic.min.css";
+import {Provider} from "mobx-react";
+
+
 
 ReactDOM.render(
   <React.StrictMode>
