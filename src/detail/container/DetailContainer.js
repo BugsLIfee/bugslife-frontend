0feb8991e75b350fccbab06d9d626022ce728519
ShimJaewook <ts4837829@naver.com>
2020-10-01 00:00:41 +0900

    Add QuestionView

--- src/detail/container/DetailContainer.js
+++ src/detail/container/DetailContainer.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
+import QuestionView from "../view/QuestionView";
+import AnswerView from "../view/AnswerView"
+import qs from "qs";
+import {withRouter} from "react-router-dom";
 
+@withRouter
 class DetailContainer extends Component {
+    
     render() {
+           const searchObj = qs.parse(this.props.location.search, {
+      ignoreQueryPrefix: true,
+    });
+            //const  id  = this.props.match.params.id;
+        console.log(",,,,,",searchObj.id);
+        console.log(",,,,,",searchObj.name);
+        console.log(",,,,,",searchObj.code);
         return (
             <div>
-                
+                <QuestionView />
+                <AnswerView />
             </div>
         );
     }
