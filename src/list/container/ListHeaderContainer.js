f54ed9d8ae58055949d4235f349762287704a2cd
0505sdj <0505sdj@gmail.com>
2020-10-22 09:13:42 +0900

    List Fix

--- src/list/container/ListHeaderContainer.js
+++ src/list/container/ListHeaderContainer.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react"
 import ListHeaderview from "../view/ListHeaderView"
+import "../view/scss/ListMain.scss"
 
 class ListHeaderContainer extends Component {
   render() {
     const { onSetOrderBy } = this.props
     return (
-      <div>
+      <div className="header">
+        <span className="qna">
+
+          <h1>Q/A</h1>
+        </span>
         <ListHeaderview onSetOrderBy={onSetOrderBy} />
       </div>
     )
--- src/list/container/ListTagContainer.js
+++ src/list/container/ListTagContainer.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react"
 import ListTagView from "../view/ListTagView"
 import { inject, observer } from "mobx-react"
+import { Button } from 'semantic-ui-react'
+import "../view/scss/ListTag.scss"
 
 @inject("Store")
 @observer
@@ -17,8 +19,18 @@ componentDidMount(){
     console.log("container taglist: ", taglist)
 
     return (
+      <div className="tagLayout">
+        <div className="addQButton">
+          <Button content='질문글 작성하기' primary className="addQButton"/>
+        </div>
+        <div className="tagBoxLayout">
+
+          <span className="tagTitle">TAG</span>
+        
       <div className="listTagContainer tagBox">
         <ListTagView tagAll={taglist} />
+      </div>
+        </div>
       </div>
     )
   }
