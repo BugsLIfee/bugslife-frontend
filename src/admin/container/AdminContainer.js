a8497131ed8f530f36a4d467ac9c19dbab8eae1a
dalsomin <dalsomin425@gmail.com>
2020-10-30 09:09:53 +0900

    sidebar scss

--- src/admin/container/AdminContainer.js
+++ src/admin/container/AdminContainer.js
@@ -2,10 +2,7 @@ import React, { Component } from "react";
 import { Grid } from "semantic-ui-react";
 import { inject, observer } from "mobx-react";
 import AdminRoutes from "../../routes/AdminRoutes";
-import "../view/scss/adminPage.scss";
-import {navStyle} from "../view/scss/adminPage.scss";
-import { Divider } from "@material-ui/core";
-import { Link, NavLink } from "react-router-dom";
+import AdminSideMenu from "../view/AdminSideMenu";
 
 @inject("Store")
 @observer
@@ -14,35 +11,16 @@ class AdminContainer extends Component {
     const match = this.props.match;
     return (
       <div class="admincontainer">
-        <div className="admin-header">👀 관리자 페이지</div>
+        <div className="admin-header">
+          <span role="img" aria-label="aria">
+            👀 관리자 페이지
+          </span>
+        </div>
         <Grid>
           <Grid.Row>
           <Grid.Column width={3} >
-            <div className="adminside">
-
-                <a href="/admin/member">
-              <div className="list">
-                  회원관리
-              </div>
-                </a>
-                <a href="/admin/report">
-              <div className="list">
-                  신고게시판
-              </div>
-                </a>
-                <a href="/admin/info">
-              <div className="list">
-                  공지사항관리
-              </div>
-                </a>
-                <a href="/admin/inquiry">
-              <div className="list">
-                  1:1문의 관리
-              </div>
-                </a>
-            </div>
+            <AdminSideMenu/>
           </Grid.Column>
-          <Divider/>
           <Grid.Column width={12}>
             <AdminRoutes match={match}/>
           </Grid.Column>
--- src/admin/view/AdminSideMenu.js
+++ src/admin/view/AdminSideMenu.js
@@ -1,103 +1,32 @@
 import React, { Component } from "react";
 import { Container,Menu ,Segment} from "semantic-ui-react";
 import {Link} from "react-router-dom";
-import "./scss/adminSide.scss";
-
-
+import "./scss/adminPage.scss";
 
 class AdminSideMenu extends Component {
-  // state = { activeItem: "member" };
-
-  // handleItemClick = (e, { name }) => this.setState({ activeItem: name });
-
   render() {
-   // const { activeItem } = this.props.menu;
     return (
-<>
-        <div className="admin-header">관리자페이지</div>
-      <div className="admin-side">
-        <div className="list">회원관리</div>
-        <div className="list">신고게시판</div>
-        <div className="list">공지사항관리</div>
-        <div className="list">1:1문의관리</div>
+    <div className="adminside">
+        <a href="/admin/member">
+          <div className="list">
+              회원관리
+          </div>
+        </a>
+        <a href="/admin/report">
+          <div className="list">
+              신고관리
+          </div>
+        </a>
+        <a href="/admin/info">
+          <div className="list info">
+              공지사항관리
+          </div>
+        </a>
 
-     
-      
-
-        {/* <Segment.Group raised>
-      
-       <Menu vertical size="large">
-
-        <Menu.Item
-          name="member"
-          //active={activeItem === "member"}
-          onClick={this.handleItemClick}
-          as={Link}
-          to="/admin/member"
-        >
-          회원관리
-        </Menu.Item>
-        <Menu.Item
-          name="member"
-         // active={activeItem === "report"}
-          onClick={this.handleItemClick}
-          as={Link}
-          to="/admin/report"
-        >
-          신고게시판 
-        </Menu.Item>
-        <Menu.Item
-          name="board"
-         // active={activeItem === "board"}
-          as={Link}
-          to="/admin/board"
-        >
-          게시물관리
-        </Menu.Item>
-        <Menu.Item
-          name="qna"
-         // active={activeItem === "qna"}
-          as={Link}
-          to="/admin/qna-all"
-          qna-a-write
-        >
-          1:1 문의 관리
-          <Menu.Menu>
-            <Menu.Item
-              name="write"
-            //  active={activeItem === "write"}
-              as={Link}
-              to="/admin/qna-all"
-            >
-              1:1 문의 내역 전체보기
-            </Menu.Item>
-          </Menu.Menu>
-        </Menu.Item>
-        <Menu.Item
-          name="info"
-        >
-          공지사항 관리
-          <Menu.Menu>
-            <Menu.Item
-              name="info-list"
-              as={Link}
-              to="/admin/info-list"
-            >
-              Search
-            </Menu.Item>
-            <Menu.Item
-              name="write"
-              as={Link}
-              to="/admin/info-write"
-            >
-              공지사항 작성
-            </Menu.Item>
-          </Menu.Menu>
-        </Menu.Item>
-      </Menu>
-      </Segment.Group> */}
-   </div>
-   </>
+          <div className="list inquiry">
+              1:1문의 관리
+          </div>
+    </div>
     );
   }
 }
