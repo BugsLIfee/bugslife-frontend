d61b99f85a6d2c496ad681319f66bd940778c024
kayaleeeee <gayeon71057@gmail.com>
2020-10-30 17:46:18 +0900

    Add responsive bar

--- src/freeBoard/view/detail/FreeboardDetailComment.js
+++ src/freeBoard/view/detail/FreeboardDetailComment.js
@@ -5,7 +5,7 @@ import Freeboarddeleteform from './FreeboardDeleteForm'
 import "./scss/FreeboarDetailComm.scss"
 
 export default class Freeboarddetailcomment extends Component {
-    state = { visible: false, comment_id: -1, delete_com: false }
+    state = { visible: false, comment_id: -1, delete_com: false, showBar: false, select_comm:-1 }
 
     onCommentClick=(id)=>{
         this.setState({visible: !this.state.visible});
@@ -17,14 +17,19 @@ export default class Freeboarddetailcomment extends Component {
         this.setState({delete_com: !this.state.delete_com});
         this.setState({visible: false});
         this.setState({comment_id: id});
+    }
 
-        // alert("댓글을 삭제하시겠습니까?")
+    onShowComm =(id)=>{
+        this.setState({showBar:!this.state.showBar, select_comm:id})
+        // console.log(this.state.select_comm)
     }
 
     render() {
         // console.log(this.state.visible, this.state)
-        
+        const select_comm= this.state.select_comm;
+        console.log(select_comm)
         const comment =this.props.comment
+        //const showBar = this.state.select_id == id && this.state.showBar? "showBar": "hideBar"
 
         return (
             <div className="freeboard_detail_comment_container">
@@ -35,7 +40,22 @@ export default class Freeboarddetailcomment extends Component {
                             <span className="freeboard_comment_info"> <h6>익명의 고수</h6> <p>{com.date}</p></span>
                             <p id="freeboard_comment_info_txt">{com.content}</p>
                         </div>
-                                
+                    
+                        <div className="freeboard_detail_comment_responsive">
+                            <h4 className= "freeboard_detail_comment_btn_responsive" onClick={()=>this.onShowComm(com.id)}><i class="fas fa-ellipsis-v"></i></h4>
+                           
+
+                            <div className={"freeboard_detail_comment_rep_btn_container " + ((select_comm === com.id) && (this.state.showBar)? "showBar": "hideBar")}>
+
+                                <div className="freeboard_detail_comment_rep_btn">
+                                    <h6 onClick={()=>this.onCommentClick(com.id)} >답글</h6>
+                                    <h6 onClick={()=>this.onCommentDelete(com.id)}> 삭제</h6>
+                                    <ReportModalContainer bt_text= {<h6>신고</h6>} />
+                                </div>
+                            </div>
+
+                        </div>
+                   
                          <div className="freeboard_detail_comment_btn">
                             <h6 onClick={()=>this.onCommentClick(com.id)} >답글</h6>
                             <h6 onClick={()=>this.onCommentDelete(com.id)}> 삭제</h6>
