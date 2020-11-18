d0453a4ceacf6c8ce29213d2fe14eb88668dfb1b
ShimJaewook <ts4837829@naver.com>
2020-11-18 17:36:17 +0900

    Setting MSA IP and Port - bugboard

--- src/bugBoardPosting/api/PostingApi.js
+++ src/bugBoardPosting/api/PostingApi.js
@@ -1,14 +1,15 @@
 import { request } from "../../oauth/api/APIUtils";
+import { BUGBOARD_API_URL } from '../../oauth/constants/index';
 
 export default class PostingApi {
 
-  url = "http://localhost:8088/api/bugboard/posting/";
+  // url = "http://localhost:8088/api/bugboard/posting/";
 
   postCreate(postingApiModel) {  
-    // postingApiModel = Object.assign({}, postingApiModel);
     console.log(postingApiModel)
     return request({
-      url: this.url,
+      // url: this.url,
+      url: BUGBOARD_API_URL +'/api/bugboard/posting/',
       method: 'POST',
       body: JSON.stringify(postingApiModel)
     });
